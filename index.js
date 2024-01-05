e4a62cf51dde252c2ee95c6864debc0f65fafd03
Hana Vavrušková <hana.vavruskova@gmail.com>
2024-01-05 10:09:46 +0100

    Styles for 840px and 1200px. Switch theme running + preference safe in local storage.

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 // https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
 
 const toggleSwitch = document.querySelector(
-  '.theme-switch input[type="checkbox"]',
+	'.theme-switch input[type="checkbox"]'
 );
 
 const switchTheme = (e) => {
-  if (e.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-  } else {
-    document.documentElement.setAttribute('data-theme', 'light');
-  }
+	if (e.target.checked) {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		localStorage.setItem('theme', 'dark');
+	} else {
+		document.documentElement.setAttribute('data-theme', 'light');
+		localStorage.setItem('theme', 'light');
+	}
 };
 
 toggleSwitch.addEventListener('change', switchTheme, false);
+
+const currentTheme = localStorage.getItem('theme')
+	? localStorage.getItem('theme')
+	: null;
+
+if (currentTheme) {
+	document.documentElement.setAttribute('data-theme', currentTheme);
+
+	if (currentTheme === 'dark') {
+		toggleSwitch.checked = true;
+	}
+}
